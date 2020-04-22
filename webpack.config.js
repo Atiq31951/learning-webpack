3c824bbc7a36c06b37a0de9d66e5c7a0bdacdba9
Atiqur Rahman <atiqur@goava.com>
2020-04-22 09:32:47 +0600

    Add gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
 const path = require('path')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },  
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
   devServer: {
-    contentBase: './dist'
-  }, 
+    contentBase: "./dist",
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
+        use: "babel-loader",
+        exclude: "/node_modules/",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        loader: ExtractTextWebpackPlugin.extract({
+          loader: "css-loader",
+        }),
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif|webp)$/,
+        use: [
+          'url-loader',
+          'image-webpack-loader'
+        ]
       }
-    ]
-  }
-}
+    ],
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('style.css')
+  ],
+};
